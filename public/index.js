6d491a96e9e2a0f9e07ef3562f77a33d229bdbcc
Fatema04 <shadrinmoni15@gmail.com>
2022-10-04 23:40:32 -0700

    responsive nav bar

---
+++ public/index.js
@@ -0,0 +1,8 @@
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu')
+burger.addEventListener('click', function () {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden')
+    }
+    else { menu.classList.add('hidden') }
+})
