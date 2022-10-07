533d51b9e64542b98f794a1b714ba994fba38b65
Fatema04 <shadrinmoni15@gmail.com>
2022-10-07 03:46:54 -0700

    README file  added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/index.html',
+  content: ['./index.html',
     './src/**/*.{html,js}',],
   theme: {
     extend: {
